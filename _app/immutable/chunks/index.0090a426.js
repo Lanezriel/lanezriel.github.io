935c1206c04c15a4501f6ac51202d1775d42f696
Lanezriel <dimitri.delbrouck@gmail.com>
2023-10-22 12:54:23 +0200

    Updates

--- _app/immutable/chunks/index.0090a426.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./index.c4d764d5.js";const o=t({visible:!1,title:"",content:"",type:"",duration:3e3});export{o as t};
---
+++ _app/immutable/chunks/index.adb6e5e9.js
@@ -0,0 +1,1 @@
+import{w as t}from"./index.3a7df3bb.js";const o=t({visible:!1,title:"",content:"",type:"",duration:3e3});export{o as t};
